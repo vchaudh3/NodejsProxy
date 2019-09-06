efb51deb8ef25c4792594046d43ae789d74125ea
Matthew Casperson <matthewcasperson@gmail.com>
2019-09-06 13:20:04 +1000

    Pass through the path to the second service

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // content of index.js
 const http = require('http');
+const url = require('url');
 const requestService = require('request');
 const port = 3030;
 const proxyUrl = process.env.PROXYURL || "https://raw.githubusercontent.com/mcasperson/NodejsProxy/master/helloworld.txt";
 
 const requestHandler = (request, response) => {
+    const path = url.parse(request.url).pathname;
     const start = new Date().getTime();
-    requestService(proxyUrl, (err, res, body) => {
+    requestService(proxyUrl + path, (err, res, body) => {
         const duration = new Date().getTime() - start;
         if (err) {
             response.end(err.toString() + " - Took " + duration + " milliseconds");
